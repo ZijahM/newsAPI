5cdcb91c9e83fa1c4ab65240d23a77eb320bfa34
ZijahM <zijah.mahmutbegovic@stu.ssst.edu.ba>
2020-12-29 22:52:53 +0100

    Add project files.

---
+++ Controllers/NewsController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using softrayAPI.Models;
+
+namespace softrayAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NewsController : Controller
+    {
+        public List<Article> vijesti = Article.articles;
+
+        [HttpGet]
+        public List<Article> Get()
+        {
+            return vijesti;
+        }
+
+        [HttpPost, Authorize]
+        public IActionResult InsertArticle (string id, string naslov, string podnaslov, string text, string img, string owner)
+        {
+            bool patched = false;
+
+            //FOR THE UPDATE METHOD
+            foreach (Article item in vijesti)
+            {
+                if (item.id==id)
+                {
+                    item.naslov = naslov;
+                    item.podnaslov = podnaslov;
+                    item.img = img;
+                    item.tekst = text;
+                    patched = true;
+                    return StatusCode(201);
+                }
+            }
+            //END OF UPDATE METHOD
+            if (patched == false)
+            {
+                Article article = new Article(id, naslov, podnaslov, text, img, owner);
+                this.vijesti.Add(article);
+                return StatusCode(201);
+            }
+           return StatusCode(201);
+        }
+
+        [HttpDelete, Authorize]
+        public IActionResult DeleteArticle(string ID)
+        {
+            this.vijesti.RemoveAll(articles => articles.id == ID);
+            return StatusCode(200);
+        }
+
+        [HttpPatch, Authorize]
+        public IActionResult EditArticle(string id, string naslov, string podnaslov, string text, string img)
+        {
+            foreach (Article item in vijesti)
+            {
+                if (item.id == id)
+                {
+                    item.naslov = naslov;
+                    item.podnaslov = podnaslov;
+                    item.tekst = text;
+                    item.img = img;
+                }
+            }
+
+            return StatusCode(200);
+        }
+    }
+}
---
+++ Models/Article.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace softrayAPI.Models
+{
+    public class Article
+    {
+        public static List<Article> articles = new List<Article>(){
+        new Article("1", "naslov", "podnaslov", "duzi tekst", "https://picsum.photos/400/500", "admin"),
+        new Article("2", "naslov2", "podnaslov2", "duzi tekst2", "https://picsum.photos/400/500", "admin"),
+        new Article("3", "naslov3", "podnaslov3", "duzi tekst3", "https://picsum.photos/400/500", "admin2"),
+    };
+        public string id { get; set; }
+        public string naslov { get; set; }
+        public string podnaslov { get; set; }
+        public string tekst { get; set; }
+        public string img { get; set; }
+        public string owner { get; set; }
+
+
+        public Article(string v1, string v2, string v3, string v4, string v5, string v6)
+        {
+            this.id = v1;
+            this.naslov = v2;
+            this.podnaslov = v3;
+            this.tekst = v4;
+            this.img = v5;
+            this.owner = v6;
+        }
+
+        
+    }
+}
